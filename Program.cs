e8cb18239d7a23b1ca037152e41895ed73d52caa
Suslov Danil <danya.sus@yandex.ru>
2021-09-21 02:48:08 +0300

    git commit -m "Add new abstract class Musician"

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 
 namespace Lab2Sharp
 {
+    abstract class Musician
+    {
+        public string Name { get; set; }
+        public string Specializathion { get; set; }
+        public string[] Repertoir { get; set; }
+        public readonly int DateOfBerth;
+        static readonly protected int date = DateTime.Now.Year;
+
+        protected abstract void LearnNewSong();
+        public abstract void GetInfo();
+    }
+
     class Program
     {
         static void Main(string[] args)
